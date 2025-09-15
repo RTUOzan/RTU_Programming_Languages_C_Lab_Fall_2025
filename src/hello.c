70b171220885b7ae4e994863d6b9f7630acc1351
RTUOzan <ozan.yurekli@edu.rtu.lv>
2025-09-15 07:53:06 +0000

    Update hello Message

--- src/hello.c
+++ src/hello.c
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[]) {
   // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  printf("Hello from Ozan yurekli!\n");
   printf("You passed %d argument(s).\n", argc - 1);
+  //it will show how many arguments we have passed with this for loop
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
   }
