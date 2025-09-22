375f4f211fb9fcc0373fb927af6b8eeea193ad6e
RTUOzan <ozan.yurekli@edu.rtu.lv>
2025-09-22 08:08:06 +0000

    Tasks  for Lab 2.

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -13,14 +13,24 @@
 
 int sum_to_n(int n) {
     // TODO: implement sum with a for loop
-    return 0; // placeholder
+    int sum = 0;
+    for (int i =1; i<= n; i++){
+        sum +=i;
+    }
+    return sum; // placeholder
 }
 
 int main(void) {
     int n;
-
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
+    if (n < 1){
+        printf("Error: n must be at least 1. \n ");
+    }else{
+        int result = sum_to_n(n);
+        printf("Sum from 1 to %d = %d\n ", n, result);
+
+    }
 
     // TODO: validate input, call function, and print result
 
--- src/lab2_2.c
+++ src/lab2_2.c
@@ -13,14 +13,24 @@
 
 long long factorial(int n) {
     // TODO: compute factorial iteratively
-    return 1; // placeholder
+    long long fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact; // placeholder
 }
 
 int main(void) {
     int n;
-
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
+
+    if (n < 0) {
+       printf("Error: n must be non negative. \n "); 
+    }else{
+        long long result = factorial(n);
+        printf("%d! = %11d\n ", n, result);     
+    }
 
     // TODO: validate input, call function, print result
 
